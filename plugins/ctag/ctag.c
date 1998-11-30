2a69f7c8f4a79e67bbeb03ca95e05271eacad3e8
Fred Leeflang <fredl@src.gnome.org>
1998-11-30 08:18:36 +0000

    Added some stuff, the plugin now reads tags files very elementary and builds a GList. The GList is walked through and every occurence of any of the tags in the main text should be colorised/hyperlinked or so, to link to the definition of the tag.

--- plugins/ctag/ctag.c
+++ plugins/ctag/ctag.c
@@ -22,45 +22,93 @@
 #include <unistd.h>
 #include <string.h>
 #include <glib.h>
+#include "ctag.h"
 
 #include "client.h"
 
 static gint context;
 
-int readtags( gchar *filename )
+void search(gpointer data, gpointer user_data)
 {
-	gchar	*dirname,*tagfile;
+	printf("searching for %s\n",((tagType *)data)->tagName);
+}
+
+int colorise( GList *list, gchar *thetext )
+{
+	printf("colorising...\n");
+	g_list_foreach (list, search, thetext);
+	return 0;
+}
+
+GList *addtag( GList *list, gchar *tagname, gchar *file_name, \
+	gchar *excmd, gchar *xflags)
+{
+	tagType	*newTag;
+
+	printf ("Adding Tag %s (%s)\n",tagname, file_name);
+	newTag = (tagType *)malloc(sizeof(tagType));
+	strcpy (newTag->tagName,tagname);
+	strcpy (newTag->fileName,file_name);
+	strcpy (newTag->exCmd,excmd);
+	strcpy (newTag->xflags,xflags);
+	list = g_list_append(list, newTag);
+	return list;
+}
+
+GList *readtags( gchar *filename, GList *taglist )
+{
+	gchar	*dirname,*tagfile, \
+		buf[1024], \
+		tagname[256],file_name[256],excmd[256],xflags[256];
+	FILE	*fp;
+	int	args;
 
 	dirname = g_dirname( filename );
 	tagfile = (gchar *)strcat( (char *)dirname, "/tags" );
-	printf ("reading %s\n", tagfile);
-	return 0;
+	if ((fp = fopen ((char *)tagfile, "r")) == NULL)
+		return NULL;
+	
+	while(fgets(buf, 1024, fp)) {
+		args = sscanf(buf, "%s\t%s\t%s\t%s\n", \
+			tagname, file_name, excmd, xflags);
+		if(!strncmp(tagname,"!_",2))
+			continue;
+		taglist = addtag (taglist, tagname, file_name, excmd, xflags);
+	}
+	fclose (fp);
+	return taglist;
 }
 
 int main( int argc, char *argv[] )
 {
 	int docid;
 	int length;
-	gchar *buff,*filename;
+	gchar *buff,*filename,*doctext;
 	client_info info = empty_info;
+	GList	*tagList=NULL;
   
 	/* Location of the plugin in the 'Plugin's' menu in gEdit */
-	info.menu_location = "[Plugins]ctag";
+	info.menu_location = "[Plugins]Ctag";
   
 	/* Initialisation of the Plugin itself, checking if being
 	   run as a plugin (ie, fromg gEdit, not from command line */	
-	context = client_init( &argc, &argv, &info );
-	docid = client_document_current( context );
-	length = strlen( buff = client_text_get( docid ));
+	context = client_init (&argc, &argv, &info);
+	docid = client_document_current (context);
+	length = strlen (buff = client_text_get (docid));
 
 	/* Get the current document's filename, we need to find the
 	   dirname */
-	filename = client_document_filename( docid );	
-   	readtags( filename ); 
-    	
-	printf("This plugin is not doing anything yet\n");
+	filename = client_document_filename (docid);	
 
-	client_finish( context );
+	/* Now get the document text */
+	doctext = client_text_get (docid);
+   	tagList = readtags (filename, tagList);
+    	
+	if (colorise (tagList, doctext))
+		printf ("couldn't colorise\n");	
+
+	g_list_free (tagList);
+	client_finish (context);
  
 	exit(0);
 }
---
+++ plugins/ctag/ctag.h
@@ -0,0 +1,11 @@
+#ifndef CTAG_H
+#define CTAG_H
+
+typedef struct _tagType {
+	gchar	tagName[256];
+	gchar	fileName[256];
+	gchar	exCmd[256];
+	gchar	xflags[256];	
+} tagType;
+
+#endif
