3b481959beafa71c4e779b61d77584777073585f
gedit <gedit>
2000-03-08 20:31:21 +0000

    Ok.. improved the plugin. it was bad before. now its uhm.. better.

--- plugins/sample/sample.c
+++ plugins/sample/sample.c
@@ -27,32 +27,19 @@ static void destroy_plugin (gE_Plugin_Data *pd)
 static void insert_hello ()
 {
 	gint i;
-	gE_view *view;
+	gE_view *view = GE_VIEW (mdi->active_view);
 	gE_document *doc = gE_document_current();
 	
-	for (i = 0; i < g_list_length (doc->views); i++) {
+		
+	i = gE_view_get_position (view);
 
-		view = g_list_nth_data (doc->views, i);
+	gtk_text_freeze (GTK_TEXT (view->text));
+	gtk_editable_insert_text (GTK_EDITABLE (view->text), "Hello World", 11, &i);
+	
 		
-		gtk_text_freeze (GTK_TEXT (view->text));
-		
-		gtk_text_set_point (GTK_TEXT(view->text), 
-							gtk_text_get_point (GTK_TEXT(view->text)));
-		gtk_text_insert (GTK_TEXT (view->text), NULL, NULL, NULL,
-				"Hello World", 11);
-		
-		gtk_text_thaw (GTK_TEXT (view->text));
+	gtk_text_thaw (GTK_TEXT (view->text));
 		
 		
-		gtk_text_freeze (GTK_TEXT (view->split_screen));
-
-		gtk_text_set_point (GTK_TEXT(view->split_screen), 
-							gtk_text_get_point (GTK_TEXT(view->text)));
-		gtk_text_insert (GTK_TEXT (view->split_screen), NULL, NULL, NULL,
-				"Hello World", 11);
-
-		gtk_text_thaw (GTK_TEXT (view->split_screen));	
-	}
 }
 	
 
