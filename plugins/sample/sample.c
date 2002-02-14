bd6b09df09c9be16ad3e157aa9cc758c0f32e975
Kjartan Maraas <kmaraas@src.gnome.org>
2002-02-14 07:12:38 +0000

    Complete broken commit

--- plugins/sample/sample.c
+++ plugins/sample/sample.c
@@ -27,6 +27,10 @@
  * See the ChangeLog files for a list of changes. 
  */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <libgnome/gnome-i18n.h>
 
 #include <gedit-menus.h>
--- plugins/spell/spell.c
+++ plugins/spell/spell.c
@@ -6,6 +6,9 @@
  *
  */
  
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <gnome.h>
 #include <glade/glade.h>
