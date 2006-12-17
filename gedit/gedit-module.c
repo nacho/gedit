76e4ad7186d0455e9da4453698e95bd2cfebaccf
Paolo Borelli <pborelli@katamail.com>
2006-12-17 14:29:49 +0000

    fail gradefully if 'register_gedit_plugin' symbol is NULL.
    2006-12-10  Paolo Borelli  <pborelli@katamail.com>
    
    	* gedit/gedit-module.c: fail gradefully if
    	'register_gedit_plugin' symbol is NULL.

--- gedit/gedit-module.c
+++ gedit/gedit-module.c
@@ -122,7 +122,15 @@ gedit_module_load (GTypeModule *gmodule)
 		return FALSE;
 	}
 
-	g_assert (register_func);
+	/* symbol can still be NULL even though g_module_symbol
+	 * returned TRUE */
+	if (register_func == NULL)
+	{
+		g_warning ("Symbol 'register_gedit_plugin' should not be NULL");
+		g_module_close (module->library);
+
+		return FALSE;
+	}
 
 	module->type = register_func (gmodule);
 
