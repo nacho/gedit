nacho/gedit

commit 6efd048552f823a3def2efcf7c5504562e374bb9

Fixed cursor going to end placeholder if there are no other placeholders
For instance, the 'main' snippet in the C language only defines an end
placeholder ($0). Upon activating the snippet, the cursor was not moved.
This has now been fixed.

plugins/snippets/snippets/Document.py
@@ -501,7 +501,7 @@ class Document:
                 self.active_snippets.append(sn)
 
                 # Put cursor at first tab placeholder
-                keys = filter(lambda x: x > 0, sn.placeholders.keys())
+                keys = filter(lambda x: x >= 0, sn.placeholders.keys())
                 
                 if len(keys) == 0:
                         buf.place_cursor(sn.begin_iter())

commit 44e27e8427e99971cd0c20d61c9c925914e96ef2

Fix regression of cursor placement after snippet insertion
This fixes a regression introduced by commit 6efd048. This previous fix
would always skip to the end placeholder even if there were other 
placeholders present.

plugins/snippets/snippets/Document.py
@@ -501,10 +501,13 @@ class Document:
                 self.active_snippets.append(sn)
 
                 # Put cursor at first tab placeholder
-                keys = filter(lambda x: x >= 0, sn.placeholders.keys())
-                
+                keys = filter(lambda x: x > 0, sn.placeholders.keys())
+
                 if len(keys) == 0:
-                        buf.place_cursor(sn.begin_iter())
+                        if 0 in sn.placeholders:
+                                self.goto_placeholder(self.active_placeholder, sn.placeholders[0])
+                        else:
+                                buf.place_cursor(sn.begin_iter())
                 else:
                         self.goto_placeholder(self.active_placeholder, sn.placeholders[keys[0]])
                 