70539309afca20efd55787741b0ef3c5c7186706
Ignacio Casal Quinteiro <icq@gnome.org>
2010-07-02 15:16:49 +0200

    Define GEDIT_LOCKDOWN_ALL outside the enum

--- gedit/gedit-app.h
+++ gedit/gedit-app.h
@@ -96,9 +96,11 @@ typedef enum
 	GEDIT_LOCKDOWN_COMMAND_LINE	= 1 << 0,
 	GEDIT_LOCKDOWN_PRINTING		= 1 << 1,
 	GEDIT_LOCKDOWN_PRINT_SETUP	= 1 << 2,
-	GEDIT_LOCKDOWN_SAVE_TO_DISK	= 1 << 3,
-	GEDIT_LOCKDOWN_ALL		= 0xF
+	GEDIT_LOCKDOWN_SAVE_TO_DISK	= 1 << 3
 } GeditLockdownMask;
+
+/* We need to define this here to avoid problems with bindings and gsettings */
+#define GEDIT_LOCKDOWN_ALL 0xF
 
 /*
  * Public methods
