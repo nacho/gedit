d5ae191600c4d100188a95dc7c789b06875542f8
Paolo Borelli <pborelli@gnome.org>
2010-07-28 23:08:33 +0200

    Fix typo

--- plugins/pythonconsole/pythonconsole/__init__.py
+++ plugins/pythonconsole/pythonconsole/__init__.py
@@ -34,7 +34,7 @@ PYTHON_ICON = 'gnome-mime-text-x-python'
 class PythonConsolePlugin(GObject.Object, Gedit.WindowActivatable, PeasUI.Configurable):
     __gtype_name__ = "PythonConsolePlugin"
 
-    window = Gobject.property(type=Gedit.Window)
+    window = GObject.property(type=Gedit.Window)
 
     def __init__(self):
         GObject.Object.__init__(self)
