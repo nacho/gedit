1d56b827f14096891bb03dbd5e8211fdad1331ad
Ignacio Casal Quinteiro <icq@gnome.org>
2010-10-03 19:47:59 +0200

    Port to latest api changes in libpeas.

--- gedit/gedit-plugins-engine.c
+++ gedit/gedit-plugins-engine.c
@@ -57,13 +57,62 @@ GeditPluginsEngine *default_engine = NULL;
 static void
 gedit_plugins_engine_init (GeditPluginsEngine *engine)
 {
+	gchar *typelib_dir;
+	GError *error = NULL;
+
 	gedit_debug (DEBUG_PLUGINS);
 
 	engine->priv = G_TYPE_INSTANCE_GET_PRIVATE (engine,
-						    GEDIT_TYPE_PLUGINS_ENGINE,
-						    GeditPluginsEnginePrivate);
+	                                            GEDIT_TYPE_PLUGINS_ENGINE,
+	                                            GeditPluginsEnginePrivate);
 
 	engine->priv->plugin_settings = g_settings_new ("org.gnome.gedit.plugins");
+	
+	/* Require gedit's typelib. */
+	typelib_dir = g_build_filename (gedit_dirs_get_gedit_lib_dir (),
+	                                "girepository-1.0",
+	                                NULL);
+
+	if (!g_irepository_require_private (g_irepository_get_default (),
+	                                    typelib_dir, "Gedit", "3.0", 0, &error))
+	{
+		g_warning ("Could not load Gedit repository: %s", error->message);
+		g_error_free (error);
+		error = NULL;
+	}
+
+	g_free (typelib_dir);
+
+	/* This should be moved to libpeas */
+	if (!g_irepository_require (g_irepository_get_default (),
+	                            "Peas", "1.0", 0, &error))
+	{
+		g_warning ("Could not load Peas repository: %s", error->message);
+		g_error_free (error);
+		error = NULL;
+	}
+
+	if (!g_irepository_require (g_irepository_get_default (),
+	                            "PeasGtk", "1.0", 0, &error))
+	{
+		g_warning ("Could not load PeasGtk repository: %s", error->message);
+		g_error_free (error);
+		error = NULL;
+	}
+	
+	peas_engine_add_search_path (PEAS_ENGINE (engine),
+	                             gedit_dirs_get_user_plugins_dir (),
+	                             gedit_dirs_get_user_plugins_dir ());
+
+	peas_engine_add_search_path (PEAS_ENGINE (engine),
+	                             gedit_dirs_get_gedit_plugins_dir (),
+	                             gedit_dirs_get_gedit_plugins_data_dir ());
+
+	g_settings_bind (engine->priv->plugin_settings,
+	                 GEDIT_SETTINGS_ACTIVE_PLUGINS,
+	                 engine,
+	                 "loaded-plugins",
+	                 G_SETTINGS_BIND_DEFAULT);
 }
 
 static void
@@ -93,72 +142,14 @@ gedit_plugins_engine_class_init (GeditPluginsEngineClass *klass)
 GeditPluginsEngine *
 gedit_plugins_engine_get_default (void)
 {
-	gchar *typelib_dir;
-	const gchar **search_paths;
-	GError *error = NULL;
-
-	if (default_engine != NULL)
+	if (default_engine == NULL)
 	{
-		return default_engine;
+		default_engine = GEDIT_PLUGINS_ENGINE (g_object_new (GEDIT_TYPE_PLUGINS_ENGINE,
+		                                                     NULL));
+
+		g_object_add_weak_pointer (G_OBJECT (default_engine),
+		                           (gpointer) &default_engine);
 	}
-
-	/* Require gedit's typelib. */
-	typelib_dir = g_build_filename (gedit_dirs_get_gedit_lib_dir (),
-					"girepository-1.0",
-					NULL);
-
-	if (!g_irepository_require_private (g_irepository_get_default (),
-					    typelib_dir, "Gedit", "3.0", 0, &error))
-	{
-		g_warning ("Could not load Gedit repository: %s", error->message);
-		g_error_free (error);
-		error = NULL;
-	}
-
-	g_free (typelib_dir);
-
-	/* This should be moved to libpeas */
-	if (!g_irepository_require (g_irepository_get_default (),
-				    "Peas", "1.0", 0, &error))
-	{
-		g_warning ("Could not load Peas repository: %s", error->message);
-		g_error_free (error);
-		error = NULL;
-	}
-
-	if (!g_irepository_require (g_irepository_get_default (),
-				    "PeasGtk", "1.0", 0, &error))
-	{
-		g_warning ("Could not load PeasGtk repository: %s", error->message);
-		g_error_free (error);
-		error = NULL;
-	}
-
-	search_paths = g_new (const gchar *, 5);
-	/* Add the user plugins dir in ~ */
-	search_paths[0] = gedit_dirs_get_user_plugins_dir ();
-	search_paths[1] = gedit_dirs_get_user_plugins_dir ();
-	/* Add the system plugins dir */
-	search_paths[2] = gedit_dirs_get_gedit_plugins_dir ();
-	search_paths[3] = gedit_dirs_get_gedit_plugins_data_dir ();
-	/* Add the trailing NULL */
-	search_paths[4] = NULL;
-
-	default_engine = GEDIT_PLUGINS_ENGINE (g_object_new (GEDIT_TYPE_PLUGINS_ENGINE,
-							     "app-name", "Gedit",
-							     "search-paths", search_paths,
-							     NULL));
-
-	g_free (search_paths);
-
-	g_object_add_weak_pointer (G_OBJECT (default_engine),
-				   (gpointer) &default_engine);
-
-	g_settings_bind (default_engine->priv->plugin_settings,
-	                 GEDIT_SETTINGS_ACTIVE_PLUGINS,
-	                 default_engine,
-	                 "loaded-plugins",
-	                 G_SETTINGS_BIND_DEFAULT);
 
 	return default_engine;
 }
